449ffabe61ddc3af12501ee388ed9e74a1568e13
TevenLeScao <teven.lescao@gmail.com>
2021-10-08 15:53:55 +0200

    moving count_occurences into file temporarily

--- gpt3_tasks/search_for_gpt3_examples.py
+++ gpt3_tasks/search_for_gpt3_examples.py
@@ -1,13 +1,37 @@
 import argparse
 import json
 import os
+import random
+import warnings
+from collections import OrderedDict
 
 import numpy as np
 from tqdm import tqdm
-import random
-from collections import OrderedDict
 
-from utils import count_occurences
+
+def count_occurences(q, suffix, tokenize=False):
+    if tokenize:
+        from transformers import GPT2Tokenizer
+        tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        arr = np.array(tokenizer.encode(q), dtype=np.uint16).view(np.uint8).tobytes()
+    else:
+        arr = q.encode('utf-8')
+    open("/tmp/fin", "wb").write(arr)
+    counted = False
+    tries = 0
+    while not counted:
+        try:
+            count = int(
+                (os.popen("../../deduplicate-text-datasets/target/debug/dedup_dataset count_occurances %s /tmp/fin" % (
+                    suffix)).read().strip().split("Number of times present: ")[-1]))
+            counted = True
+        except ValueError:
+            tries += 1
+            if tries == 5:
+                count = 0
+                warnings.warn(f"Failed to count query {q}")
+                break
+    return count
 
 
 def build_reverse(word, **kwargs):
@@ -38,7 +62,7 @@ def build_cycles(word, **kwargs):
 def build_insertion(word, chars=None, n_outputs=5, **kwargs):
     if chars is None:
         chars = [",", ".", "!", "/", "\\", ":", ";", " "]
-    return {"".join(i + chars[random.randint(0, len(chars)-1)] for i in word) for _ in range(n_outputs)}
+    return {"".join(i + chars[random.randint(0, len(chars) - 1)] for i in word) for _ in range(n_outputs)}
 
 
 if __name__ == "__main__":
@@ -74,6 +98,7 @@ if __name__ == "__main__":
                       tr_fn(word, n_outputs=args.n_outputs) if query != word}
             tr_counts[word] = counts
         average_count = np.mean([count for word_count in tr_counts.values() for count in word_count.values()])
-        json.dump(tr_counts, open(os.path.join(args.output_folder, f"{tr_name}_per_word_count.json"), "w"), ensure_ascii=False, indent=2)
-        json.dump(average_count, open(os.path.join(args.output_folder, f"{tr_name}_avg_count.json"), "w"), ensure_ascii=False, indent=2)
-
+        json.dump(tr_counts, open(os.path.join(args.output_folder, f"{tr_name}_per_word_count.json"), "w"),
+                  ensure_ascii=False, indent=2)
+        json.dump(average_count, open(os.path.join(args.output_folder, f"{tr_name}_avg_count.json"), "w"),
+                  ensure_ascii=False, indent=2)
