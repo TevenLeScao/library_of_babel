02c7e538c453dead3bb83f74b4ea87f78fa1c1f0
TevenLeScao <teven.lescao@gmail.com>
2021-10-13 15:59:08 +0200

    fixed_length_search.py

--- prompt_deduplication/fixed_length_search.py
+++ prompt_deduplication/fixed_length_search.py
@@ -1,10 +1,34 @@
 import argparse
 import json
 import os
+import warnings
 
+import numpy as np
 from tqdm import tqdm
 
-from utils import count_occurences
+
+def count_occurences(q, suffix, tokenize=False):
+    if tokenize:
+        from transformers import GPT2Tokenizer
+        tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        arr = np.array(tokenizer.encode(q), dtype=np.uint16).view(np.uint8).tobytes()
+    else:
+        arr = q.encode('utf-8')
+    open("/tmp/fin", "wb").write(arr)
+    counted = False
+    tries = 0
+    while not counted:
+        try:
+            count = int((os.popen("../../deduplicate-text-datasets/target/debug/dedup_dataset count_occurances %s /tmp/fin" % (
+                suffix)).read().strip().split("Number of times present: ")[-1]))
+            counted = True
+        except ValueError:
+            tries += 1
+            if tries == 5:
+                count = 0
+                warnings.warn(f"Failed to count query {q}")
+                break
+    return count
 
 
 def fixed_length_search(top_level_query, suffix, substring_length):
